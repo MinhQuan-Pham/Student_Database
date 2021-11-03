57ee5b70ed6ad66bf3f374ff5ca3561505f36e8b
Minh-Quan Pham <65067055+MinhQuan-Pham@users.noreply.github.com>
2021-11-03 16:11:40 -0400

    Add files via upload

---
+++ StudentInformation.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+
+namespace StudentDatabase
+{
+    public class Student
+    {
+        public static void AddRecord(string ID, string name, string email, string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter sw = new StreamWriter(File.Create(filePath))) //If file doesn't exists, create new file
+                {
+                    sw.WriteLine(ID + "," + name + "," + email);
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter w = File.AppendText(filePath)) //If file exists, append
+                {
+                    w.WriteLine(ID + "," + name + "," + email);
+                    w.Close();
+                }
+            }
+
+        }
+
+        public static string[] FindRecord(string searchTerm, string filePath, int indexOfSearchField)
+        {
+            string[] recordNotFound = { "Record not found" };
+            string[] lines = File.ReadAllLines(filePath);
+
+
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(',');
+                if (RecordMatches(searchTerm, fields, indexOfSearchField))
+                {
+                    Console.WriteLine("Record Found");
+                    Console.WriteLine("ID, Name, Email");
+                    return fields;
+                }
+            }
+
+            return recordNotFound;
+        }
+
+        public static bool RecordMatches(string searchTerm, string[] record, int indexOfSearchField)
+        {
+            if (record[indexOfSearchField].Equals(searchTerm))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void DeleteRecord(string searchTerm, string filePath, int indexOfSearchField)
+        {
+            string tempFile = @"student_information.csv";
+            
+            string[] lines = File.ReadAllLines(filePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(',');
+
+                if (!(RecordMatches(searchTerm, fields, indexOfSearchField)))
+                {
+                    AddRecord(fields[0], fields[1], fields[2], tempFile);
+                }
+                else
+                {
+                    Console.WriteLine("Record Deleted");
+                }
+            }
+
+            File.Delete(filePath); //delete old file
+            File.Move(tempFile, filePath);
+        }
+
+        public static void EditRecord(string searchTerm, string filePath, int indexOfSearchField)
+        {
+            string tempFile = @"student_information.csv";
+            
+            string[] lines = File.ReadAllLines(filePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(',');
+
+                if (!(RecordMatches(searchTerm, fields, indexOfSearchField)))
+                {
+                    AddRecord(fields[0], fields[1], fields[2], tempFile);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the edited information");
+                    Console.WriteLine("New ID");
+                    string editID = Console.ReadLine();
+                    Console.WriteLine("New Name");
+                    string editName = Console.ReadLine();
+                    Console.WriteLine("New Email");
+                    string editEmail = Console.ReadLine();
+
+                    AddRecord(editID, editName, editEmail, tempFile);
+                    Console.WriteLine("Record Edited");
+                }
+            }
+
+            File.Delete(filePath); //delete old file
+            File.Move(tempFile, filePath);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace StudentDatabase
+{
+    public class User
+    {
+        public string userName;
+        public string password;
+
+        public static void SignUp()
+        {
+            Console.WriteLine("--- Signing Up ---");
+            User user1 = new User();
+
+            Console.WriteLine("Please enter your User Name");
+            user1.userName = Console.ReadLine();
+            Console.WriteLine("Please enter your Password");
+            user1.password = Console.ReadLine();
+
+            using (StreamWriter sw = new StreamWriter(File.Create("/Users/mike/Projects/StudentDatabase/user_information.csv")))
+            {
+                sw.WriteLine(user1.userName);
+                sw.WriteLine(user1.password);
+                sw.Close();
+            }
+
+            Console.WriteLine("Sign Up Successfully");
+        }
+
+        public static bool Login()
+        {
+            Console.WriteLine("--- Login ---");
+            Console.WriteLine("User Name: ");
+            string userNameCheck = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            string passwordCheck = Console.ReadLine();
+
+            User userOriginal = new User();
+
+            using (StreamReader sr = new StreamReader(File.Open("/Users/mike/Projects/StudentDatabase/user_information.csv", FileMode.Open)))
+            {
+                userOriginal.userName = sr.ReadLine();
+                userOriginal.password = sr.ReadLine();
+                sr.Close();
+            }
+
+            if (userNameCheck == userOriginal.userName && passwordCheck == userOriginal.password)
+            {
+                Console.WriteLine("Login Successfully");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Login Failed");
+                return false;
+            }
+
+        }
+    }
+}
